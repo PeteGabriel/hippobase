78bd3076a74e1cd9fa15365fdcc533261ef2921b
PeteGabriel <pedrogabriel@protonmail.ch>
2024-06-21 18:14:59 +0200

    Add json tags

--- types.go
+++ types.go
@@ -2,9 +2,11 @@ package equestrian_events_riders_list
 
 // RidersEntryRow represents a row in the entry list of riders for a certain event.
 type RidersEntryRow struct {
-	Flag, CountryCode, CountryName string
+	Flag        string `json:"flag"`
+	CountryCode string `json:"country_code"`
+	CountryName string `json:"country_name"`
 	// horse rider - set of horses
-	Pairs map[string][]string
+	Pairs map[string][]string `json:"pairs"`
 }
 
 // EventEntryRow represents a row in the minified event table.
@@ -15,10 +17,10 @@ type EventEntryRow struct {
 // EventInfo represents the complete information of an event.
 // It contains the event name, the date of creation and the entry list of competitors.
 type EventInfo struct {
-	CreatedAt,
-	EventFullName string
-	Competitors []RidersEntryRow
-	TotalNations,
-	TotalAthletes,
-	TotalHorses int
+	CreatedAt     string           `json:"created_at"`
+	EventFullName string           `json:"event_name"`
+	Competitors   []RidersEntryRow `json:"competitors,omitempty"`
+	TotalNations  int              `json:"total_nations"`
+	TotalAthletes int              `json:"total_athletes"`
+	TotalHorses   int              `json:"total_horses"`
 }
